f1e4aeaa8f4c6160e21d5d22d5c5c4cf4408eb38
aspizu <aspizu@protonmail.com>
2025-07-20 12:49:35 +0530

    feat: add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import {defineConfig} from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://aspizu.github.io",
+    base: "projects",
     vite: {
         plugins: [tailwindcss()],
     },
